d85331eaeafeb6ad411e1c31f7d42009c672e01b
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2024-06-09 16:48:30 +0530

    Update activations.py

--- activations.py
+++ activations.py
@@ -22,7 +22,7 @@ class Activation(Layer):
         self.input = input
         return self.activation(self.input)
 
-    def backward(self, output_gradient, learning_rate):
+    def backward(self, output_gradient):
         return np.multiply(output_gradient, self.activation_prime(self.input))
 
 # activation functions
@@ -73,7 +73,7 @@ class Softmax(Layer):
         self.output = tmp / np.sum(tmp)
         return self.output
     
-    def backward(self, output_gradient, learning_rate):
+    def backward(self, output_gradient):
         # This version is faster than the one presented in the video
         n = np.size(self.output)
         return np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
