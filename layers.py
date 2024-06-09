29d21b48d9001474c2c57ce7f40fbadf06c5beec
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2024-06-09 16:48:09 +0530

    Update layers.py

--- layers.py
+++ layers.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Layer:
     def __init__(self):
         self.input = None
@@ -18,17 +17,18 @@ class Dense(Layer):
     def __init__(self,input_size,output_size):
         self.weights = np.random.randn(input_size,output_size)
         self.bias = np.random.randn(output_size,1)
+        self.weights_gradient = None
+        self.bias_gradient = None
     
     def forward(self, input):
         self.input = input
         self.output = np.dot(self.weights.T, self.input) + self.bias
         return self.output
 
-    def backward(self, output_gradient, learning_rate):
-        weights_gradient = np.dot(self.input, output_gradient.T)
+    def backward(self, output_gradient):
+        self.weights_gradient = np.dot(self.input, output_gradient.T)
+        self.bias_gradient = output_gradient
         input_gradient = np.dot(self.weights, output_gradient)
-        self.weights -= learning_rate * weights_gradient
-        self.bias -= learning_rate * output_gradient
         return input_gradient
 
 
