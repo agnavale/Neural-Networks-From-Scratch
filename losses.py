0d0bddd59bc239ea788d8e842872e2c40a765c16
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2024-06-09 16:47:32 +0530

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,18 +1,32 @@
 import numpy as np
 
-def mse(y_true, y_pred):
-    return np.mean(np.power(y_true - y_pred, 2))
+class Loss:
+    def forward(self, y_true, y_pred):
+        # TODO: returns mean loss
+        pass
 
-def mse_prime(y_true, y_pred):
-    return 2 * (y_pred - y_true) / np.size(y_true)
+    def backward(self, y_true, y_pred):
+        # TODO: returns loss_prime vector
+        pass
 
-def cross_entropy(y_true, y_pred):
-    y_pred_clipped = np.clip(y_pred, 1e-8,1-1e-8)
-    return np.mean(-y_true * np.log(y_pred_clipped))
+# losses
+class mse(Loss):
+    def forward(self,y_true, y_pred):
+        return np.mean(np.power(y_true - y_pred, 2))
 
-def cross_entropy_prime(y_true, y_pred):
-    y_pred_clipped = np.clip(y_pred, 1e-8,1-1e-8)
-    return (-y_true/ y_pred_clipped) / np.size(y_true)
+    def backward(self,y_true, y_pred):
+        return 2 * (y_pred - y_true) / np.size(y_true)
+
+class cross_entropy(Loss):
+    def forward(self,y_true, y_pred):
+        y_pred_clipped = np.clip(y_pred, 1e-8,1-1e-8)
+        return np.mean(-y_true * np.log(y_pred_clipped))
+
+    def backward(self,y_true, y_pred):
+        y_pred_clipped = np.clip(y_pred, 1e-8,1-1e-8)
+        return (-y_true/ y_pred_clipped) / np.size(y_true)
+
+
 
 
 
