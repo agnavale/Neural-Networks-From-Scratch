4685fc612922bb1d17b00978e7d19964f0737f25
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2024-06-09 16:46:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import numpy as np
 from layers import Dense
 from models import Sequential
 from activations import Softmax
-from losses import cross_entropy,cross_entropy_prime
 
 # 2 to 4 decoder , classification problem
 X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
 Y = np.reshape([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]], (4, 4, 1))
-
 
 model = Sequential()
 model.add(Dense(input_size=2,output_size=10))
@@ -15,12 +13,14 @@ model.add(Softmax())
 model.add(Dense(input_size=10,output_size=4))
 model.add(Softmax())
 
-model.compile(loss='cross_entropy')
-model.fit(X,Y, epochs = 10000, learning_rate= 0.01)
+model.compile(loss='cross_entropy', optimizer="Momentum", metric=None)
+model.fit(X,Y, epochs = 10000)
 
-x1 = 0
-while x1!= 100:
+while True:
     x1 = int(input("x1: "))
+    if x1 == -1:
+        break
+        
     x2 = int(input("x2: "))
     x = np.reshape([x1,x2],(2,1))
     print(model.predict(x))
