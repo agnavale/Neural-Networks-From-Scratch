03687a988aa63836d262d8d1731a3686bf6a97b1
Atharv Navale <109289472+agnavale@users.noreply.github.com>
2024-06-12 09:44:31 +0530

    Update main.py

--- batch_gradient/main.py
+++ batch_gradient/main.py
@@ -9,6 +9,7 @@ Y = np.array( [[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]] )
 model = Sequential()
 model.add(Dense(2,5,activation="Softmax"))
 model.add(Dense(5,4,activation="Softmax"))
+
 model.compile(loss="Cross_entropy", optimizer="SGD", metric= None)
 model.fit(X,Y, batch_size=1, epochs=10000)
 
